6ac3c110d91385ebb5d7569f1a7871f8b09670e2
Pridemore <pridemoreviriri@gmail.com>
2021-06-30 19:41:59 +0200

    pointing to database on the server and migrating to Sdk version 28.

--- app/src/main/java/com/pamsillah/wakho/app_settings/ConnectionConfig.java
+++ app/src/main/java/com/pamsillah/wakho/app_settings/ConnectionConfig.java
@@ -1,5 +1,6 @@
 package com.pamsillah.wakho.app_settings;
 
 public class ConnectionConfig {
-    public static final String BASE_URL="http://87baef65f0a6.ngrok.io";
+    //public static final String BASE_URL="http://87baef65f0a6.ngrok.io";
+    public static final String BASE_URL="http://wakho.com/service";
 }
